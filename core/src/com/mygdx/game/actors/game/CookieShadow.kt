3a406793d9834c5e47c232c0fafab2d1c8df9e4d
Art-_-master <ggn.master@gmail.com>
2020-06-20 15:55:19 +0400

    (fix) fix cookie shadow

--- core/src/com/mygdx/game/actors/game/CookieShadow.kt
+++ core/src/com/mygdx/game/actors/game/CookieShadow.kt
@@ -6,14 +6,12 @@ import com.mygdx.game.api.GameActor
 import com.mygdx.game.data.Assets
 import com.mygdx.game.data.Descriptors
 
-class CookieShadow(manager : AssetManager,
-                   private val cookie: Cookie): GameActor() {
+class CookieShadow(manager : AssetManager, private val cookie: Cookie): GameActor() {
 
     private val texture = manager.get(Descriptors.environment)
     private val region = texture.findRegion(Assets.EnvironmentAtlas.CIRCLE_SHADOW)
     private val origWidth = region.originalWidth.toFloat()
     private val origHeight = region.originalHeight.toFloat()
-    private var peak = cookie.ground
 
     init {
         width = origWidth
@@ -22,34 +20,31 @@ class CookieShadow(manager : AssetManager,
 
     override fun act(delta: Float) {
         super.act(delta)
-        calculateShadowSize()
+        setShadow()
     }
 
-    private fun calculateShadowSize(){
-        val widthChange = 1f
-        val heightChange = 0.4f
+    private fun setShadow(){
         setShadowOpacity()
-        if(cookie.y > peak){
-            peak = cookie.y
-            width -= widthChange
-            height -= heightChange
-        } else if(cookie.isFalling()){
-            width += widthChange
-            height += heightChange
-        } else if(cookie.isRun() && cookie.y == cookie.startY){
-            if(cookie.y <= cookie.ground) peak = cookie.ground
-            width = origWidth
-            height = origHeight
+        setShadowSize()
+    }
+
+    private fun setShadowSize(){
+        val maxSizePoint = 400
+        val percent = 100 - ((cookie.y - cookie.startY ) / (maxSizePoint / 100))
+        if(percent > 0 && percent <= 100){
+            println(percent)
+            width = region.originalWidth / (100 / percent)
+            height = region.originalHeight / (100 / percent)
         }
     }
 
     private fun setShadowOpacity(){
         val invisiblePoint = 100
-        val shadowOpacity = 100 - ((cookie.y - cookie.startY ) / (invisiblePoint / 100))
+        val percent = 100 - ((cookie.y - cookie.startY ) / (invisiblePoint / 100))
         val alpha = when {
-            shadowOpacity / 100 > 1 -> 1f
-            shadowOpacity / 100 < 0 -> 0f
-            else -> shadowOpacity / 100
+            percent / 100 > 1 -> 1f
+            percent / 100 < 0 -> 0f
+            else -> percent / 100
         }
         color.a = alpha
     }
@@ -58,6 +53,6 @@ class CookieShadow(manager : AssetManager,
         val x = cookie.x
         val y = cookie.startY - 20
         batch!!.color = color
-        batch.draw(region, x, y, x, y,  width, height, scaleX, scaleY, rotation)
+        batch.draw(region, x, y, 0f, 0f,  width, height, scaleX, scaleY, rotation)
     }
 }
