88f9b18c791e40788ac405ff378b8d2c898d8acc
Art-_-master <ggn.master@gmail.com>
2021-02-13 11:36:10 +0400

    (fix) change texture filters

--- core/src/com/run/cookie/run/game/data/Descriptors.kt
+++ core/src/com/run/cookie/run/game/data/Descriptors.kt
@@ -2,7 +2,6 @@ package com.run.cookie.run.game.data
 
 import com.badlogic.gdx.assets.AssetDescriptor
 import com.badlogic.gdx.assets.AssetLoaderParameters
-import com.badlogic.gdx.assets.loaders.TextureAtlasLoader
 import com.badlogic.gdx.assets.loaders.TextureLoader
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
@@ -14,19 +13,7 @@ object Descriptors {
         genMipMaps = true
         loadedCallback = AssetLoaderParameters.LoadedCallback { assetManager, fileName, type ->
             run {
-                assetManager.get<Texture>(fileName).
-                setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest)
-            }
-        }
-    }
-
-    private val atlasParams = TextureAtlasLoader.TextureAtlasParameter().apply {
-        this.
-        loadedCallback = AssetLoaderParameters.LoadedCallback { assetManager, fileName, type ->
-            run {
-                assetManager.get<TextureAtlas>(fileName).textures.forEach {
-                    it.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest)
-                }
+                assetManager.get<Texture>(fileName).setFilter(Texture.TextureFilter.MipMap, Texture.TextureFilter.Nearest)
             }
         }
     }
@@ -34,10 +21,10 @@ object Descriptors {
 
     val background = AssetDescriptor(Assets.BackgroundTexture.NAME, Texture::class.java, textureParams)
     val gameOverBackground = AssetDescriptor(Assets.GameOverBackground.NAME, Texture::class.java, textureParams)
-    val comics = AssetDescriptor(Assets.ComicsAtlas.NAME, TextureAtlas::class.java, atlasParams)
-    val cookie = AssetDescriptor(Assets.CookieAtlas.NAME, TextureAtlas::class.java, atlasParams)
-    val environment = AssetDescriptor(Assets.EnvironmentAtlas.NAME, TextureAtlas::class.java, atlasParams)
-    val menu = AssetDescriptor(Assets.MainMenuAtlas.NAME, TextureAtlas::class.java, atlasParams)
+    val comics = AssetDescriptor(Assets.ComicsAtlas.NAME, TextureAtlas::class.java)
+    val cookie = AssetDescriptor(Assets.CookieAtlas.NAME, TextureAtlas::class.java)
+    val environment = AssetDescriptor(Assets.EnvironmentAtlas.NAME, TextureAtlas::class.java)
+    val menu = AssetDescriptor(Assets.MainMenuAtlas.NAME, TextureAtlas::class.java)
     val progressBar = AssetDescriptor(Assets.ProgressAtlas.NAME, TextureAtlas::class.java)
 
     val scoreFont = AssetDescriptor(FontParam.SCORE.fontName, BitmapFont::class.java, FontParam.SCORE.get())
